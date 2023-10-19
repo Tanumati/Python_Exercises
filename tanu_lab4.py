883b06c2760077cafff25bff42e011b5886e56de
Tanu patel <tanoost19@gmail.com>
2023-10-19 12:58:07 +0100

    first commit

---
+++ tanu_lab4.py
@@ -0,0 +1,32 @@
+myFruitList = ["apple","banana", "cherry"]
+#tanu
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2]= "orange"
+print(myFruitList)
+myFinalAnswerTruple =("apple","banana","pineapple")
+print(myFinalAnswerTruple)
+print(type(myFinalAnswerTruple))
+print(myFinalAnswerTruple[0])
+print(myFinalAnswerTruple[1])
+print(myFinalAnswerTruple[2])
+
+myfavoriteFruitDictionary ={
+    "Akua":"apple",
+    "Saanvi":"banana",
+    "paulo":"pineaple"
+
+}
+print(myfavoriteFruitDictionary)
+print(type(myfavoriteFruitDictionary))
+print(myfavoriteFruitDictionary["Akua"])
+print(myfavoriteFruitDictionary["paulo"])
+print(myfavoriteFruitDictionary["Saanvi"])
+
+
+
+
+
